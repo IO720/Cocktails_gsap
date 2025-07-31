e7144129e062bdb9351ea05d91ae212ae2bcb991
IO720 <IO_720@protonmail.com>
2025-07-31 13:00:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, SplitText);
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
