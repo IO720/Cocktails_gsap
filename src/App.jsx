c8654daf2812903207565caa07fc093af0cd2e1a
IO720 <IO_720@protonmail.com>
2025-08-01 22:13:08 +0530

    Hero layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { SplitText } from "gsap/SplitText";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, SplitText);
 
@@ -12,6 +13,8 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero/>
+      <div className="h-dvh bg-black"></div>
     </main>
   );
 };
